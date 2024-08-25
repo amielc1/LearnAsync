2cd33ce5792a2fff26336684e55d6f9727339b60
Amiel Cohen <amielc1@gmail.com>
2024-08-25 22:22:52 +0300

    Add bloom filter p1 #2

--- LearnAsync/ChunkProcessor.cs
+++ LearnAsync/ChunkProcessor.cs
@@ -1,32 +1,43 @@
-﻿
-namespace LearnAsync;
-public class ChunkProcessor
+﻿using BloomFilter;
+
+namespace LearnAsync
 {
-    private readonly SemaphoreSlim _semaphore = new SemaphoreSlim(1, 1);
-
-    public async Task ProcessChunkAsync(List<string> lines, string outputFilePath)
+    public class ChunkProcessor
     {
-        await _semaphore.WaitAsync();
+        private readonly SemaphoreSlim _semaphore = new SemaphoreSlim(1, 1);
+        private readonly IBloomFilter _bloomFilter;
 
-        try
-        {
-            await WriteUniqueLinesToFile(lines, outputFilePath);
+        public ChunkProcessor(int expectedItemCount = 10000000, double falsePositiveRate= 0.01)
+        { 
+            _bloomFilter = FilterBuilder.Build(10000000, 0.01);
         }
-        finally
-        {
-            _semaphore.Release();
-        }
-    }
 
-    private async Task WriteUniqueLinesToFile(List<string> lines, string outputFilePath)
-    {
-        var uniqueLines = new HashSet<string>(lines);
-
-        using (StreamWriter writer = new StreamWriter(outputFilePath, append: true))
+        public async Task ProcessChunkAsync(List<string> lines, string outputFilePath)
         {
-            foreach (var line in uniqueLines)
+            await _semaphore.WaitAsync();
+
+            try
             {
-                await writer.WriteLineAsync(line);
+                await WriteUniqueLinesToFile(lines, outputFilePath);
+            }
+            finally
+            {
+                _semaphore.Release();
+            }
+        }
+
+        private async Task WriteUniqueLinesToFile(List<string> lines, string outputFilePath)
+        {
+            using (StreamWriter writer = new StreamWriter(outputFilePath, append: true))
+            {
+                foreach (var line in lines)
+                {
+                    if (!_bloomFilter.Contains(line))
+                    {
+                        _bloomFilter.Add(line);
+                        await writer.WriteLineAsync(line);
+                    }
+                }
             }
         }
     }
